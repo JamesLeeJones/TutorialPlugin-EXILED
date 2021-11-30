d6323067a687bc1f687afbe3a25af07c0a0041fe
nch33 <nch33@LAPTOP-P7VK2NDC>
2021-11-30 11:47:26 -0500

    Add project files.

---
+++ TutorialPlugin/TutorialPlugin.cs
@@ -0,0 +1,29 @@
+﻿namespace TutorialPlugin;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using System;
+
+public class Tutorial : Plugin<Config>
+{
+
+    private static readonly Tutorial Wow = new Tutorial();
+
+    public static Tutorial Instance => Wow;
+
+    private Tutorial()
+    {
+    }
+
+    public override PluginPriority Priority { get; } = PluginPriority.Last;
+    public override void OnEnabled()
+    {
+        RegisterEvents();
+
+        Log.Warn($"I correctly read the string config, its value is: {Config.String}");
+        Log.Warn($"I correctly read the int config, its value is: {Config.Int}");
+        Log.Warn($"I correctly read the float config, its value is: {Config.Float}");
+
+        base.OnEnabled();
+    }
+
+}
